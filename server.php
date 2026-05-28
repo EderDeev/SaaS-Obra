62d2f51ba1492b8fc7c382c61e99c2c4cbb36b7f
Ederson Moreira <eder.dev1@gmail.com>
2026-05-28 11:00:49 -0300

    Fix static asset MIME types

--- server.php
+++ server.php
@@ -7,6 +7,31 @@ $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? ''
 );
 
+function static_file_content_type(string $file): string
+{
+    $extension = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+
+    return match ($extension) {
+        'css' => 'text/css; charset=UTF-8',
+        'js', 'mjs' => 'application/javascript; charset=UTF-8',
+        'json', 'map' => 'application/json; charset=UTF-8',
+        'svg' => 'image/svg+xml',
+        'png' => 'image/png',
+        'jpg', 'jpeg' => 'image/jpeg',
+        'gif' => 'image/gif',
+        'webp' => 'image/webp',
+        'ico' => 'image/x-icon',
+        'woff' => 'font/woff',
+        'woff2' => 'font/woff2',
+        'ttf' => 'font/ttf',
+        'otf' => 'font/otf',
+        'pdf' => 'application/pdf',
+        'zip' => 'application/zip',
+        'wasm' => 'application/wasm',
+        default => mime_content_type($file) ?: 'application/octet-stream',
+    };
+}
+
 if ($uri !== '/') {
     $file = realpath($publicPath.$uri);
 
@@ -19,7 +44,7 @@ if ($uri !== '/') {
         && is_file($file);
 
     if ($isPublicFile || $isPublicStorageFile) {
-        header('Content-Type: '.(mime_content_type($file) ?: 'application/octet-stream'));
+        header('Content-Type: '.static_file_content_type($file));
         header('Content-Length: '.filesize($file));
         readfile($file);
 
